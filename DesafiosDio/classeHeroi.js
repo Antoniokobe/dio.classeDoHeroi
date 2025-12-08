4169bca2477d7ed1c60bfb0602b18a53fa010f91
Antoniokobe <antonioconceicao!outlook.com>
2025-12-08 14:07:55 -0300

    Desafio Completo

--- DesafiosDio/classeHeroi.js
+++ DesafiosDio/classeHeroi.js
@@ -1,28 +1,25 @@
-// 1. Declaração das variáveis
-let nomeHeroi = "Gandalf"; // Você pode mudar o nome aqui
-let xpHeroi = 7500;        // Você pode mudar o XP aqui
+// Array de heróis (simulando múltiplas entradas)
+const herois = [
+    { nome: "Arthur", xp: 500 },
+    { nome: "Lancelot", xp: 3000 },
+    { nome: "Merlin", xp: 12000 }
+];
 
-// 2. Variável para armazenar o nível
-let nivel;
+// LAÇO DE REPETIÇÃO: percorre todos os heróis
+for (let i = 0; i < herois.length; i++) {
+    const heroi = herois[i];
+    let nivel;
 
-// 3. Estrutura de decisão para classificar o nível
-if (xpHeroi < 1000) {
-    nivel = "Ferro";
-} else if (xpHeroi <= 2000) { // entre 1001 e 2000
-    nivel = "Bronze";
-} else if (xpHeroi <= 5000) { // entre 2001 e 5000
-    nivel = "Prata";
-} else if (xpHeroi <= 7000) { // entre 5001 e 7000
-    nivel = "Ouro";
-} else if (xpHeroi <= 8000) { // entre 7001 e 8000
-    nivel = "Platina";
-} else if (xpHeroi <= 9000) { // entre 8001 e 9000
-    nivel = "Ascendente";
-} else if (xpHeroi <= 10000) { // entre 9001 e 10000
-    nivel = "Imortal";
-} else { // maior ou igual a 10001
-    nivel = "Radiante";
+    // ESTRUTURA DE DECISÃO
+    if (heroi.xp < 1000) nivel = "Ferro";
+    else if (heroi.xp <= 2000) nivel = "Bronze";
+    else if (heroi.xp <= 5000) nivel = "Prata";
+    else if (heroi.xp <= 7000) nivel = "Ouro";
+    else if (heroi.xp <= 8000) nivel = "Platina";
+    else if (heroi.xp <= 9000) nivel = "Ascendente";
+    else if (heroi.xp <= 10000) nivel = "Imortal";
+    else nivel = "Radiante";
+
+    // VARIÁVEIS sendo usadas
+    console.log(`O Herói de nome **${heroi.nome}** está no nível de **${nivel}**`);
 }
-
-// 4. Saída final
-console.log(`O Herói de nome **${nomeHeroi}** está no nível de **${nivel}**`);
